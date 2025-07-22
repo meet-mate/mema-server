d6f492a87fcd350d2caeb7fa8bbe75b99aa6a1c7
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-07-22 16:03:35 +0900

    first commit

---
+++ module-app/src/main/java/com/mema/server/MemaServerApplication.java
@@ -0,0 +1,13 @@
+package com.mema.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemaServerApplication.class, args);
+	}
+
+}
---
+++ module-app/src/test/java/com/mema/server/MemaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mema.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
