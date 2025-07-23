98a0ccb10ba198e338143db47f5d4aa3d799cf08
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-07-23 17:53:59 +0900

    refactor(Global): Error 응답 형식 수정 (#2) - ErrorResponse 인터페이스 정의 - Binding result 관련 코드 삭제 - default 클래스 정의

--- module-app/src/main/java/com/mema/server/global/exception/ErrorResponse.java
+++ module-app/src/main/java/com/mema/server/global/exception/ErrorResponse.java
@@ -1,51 +1,34 @@
 package com.mema.server.global.exception;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
- * <b>에러 응답 DTO</b>
+ * <b>공통 에러 응답 인터페이스</b>
  *
+ * <p>각 도메인의 에러 응답 클래스는 해당 인터페이스를 implements 하여 구현한다.</p>
  */
-public record ErrorResponse(
+public interface ErrorResponse{
+    String code();
+    String message();
+    int status();
+    String path();
+    LocalDateTime timestamp();
+
+    public static ErrorResponse of(ErrorCode e, HttpServletRequest req) {
+        return new DefaultErrorResponse(
+                e.getCode(),
+                e.getMessage(),
+                e.getStatus().value(),
+                req.getRequestURI(),
+                LocalDateTime.now());
+    }
+}
+record DefaultErrorResponse(
         String code,
         String message,
         int status,
         String path,
-        LocalDateTime timestamp,
-        @JsonInclude(JsonInclude.Include.NON_EMPTY) List<FieldError> fieldErrors) {
-
-    /**
-     * Bean Validation의 필드 오류 정보를 담기 위한 서브 레코드.
-     *
-     * @param field  오류가 난 필드명
-     * @param reason 오류 메시지
-     */
-    public record FieldError(String field, String reason) {}
-
-    /** 일반 예외용 팩토리 메서드 */
-    public static ErrorResponse of(ErrorCode e, HttpServletRequest req) {
-        return new ErrorResponse(
-                e.getCode(),
-                e.getMessage(),
-                e.getStatus().value(),
-                req.getRequestURI(),
-                LocalDateTime.now(),
-                List.of());
-    }
-
-    /** Validation 실패용 팩토리 메서드 */
-    public static ErrorResponse ofValidation(
-            ErrorCode e, HttpServletRequest req, List<FieldError> errors) {
-        return new ErrorResponse(
-                e.getCode(),
-                e.getMessage(),
-                e.getStatus().value(),
-                req.getRequestURI(),
-                LocalDateTime.now(),
-                errors);
-    }
-}
+        LocalDateTime timestamp
+) implements ErrorResponse {}
