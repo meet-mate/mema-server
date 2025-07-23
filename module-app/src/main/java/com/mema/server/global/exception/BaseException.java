af9d05764149fa4446b078abdd93d3a408a10b87
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-07-23 17:52:29 +0900

    refactor(Global): 공통 Exception abstract class 정의 (#2) - 기존 ServiceException 클래스 삭제 - 공통 Exception 클래스 정의 - default Exception class 정의

---
+++ module-app/src/main/java/com/mema/server/global/exception/BaseException.java
@@ -0,0 +1,43 @@
+package com.mema.server.global.exception;
+
+import lombok.Getter;
+
+/**
+ * <b>공통 예외 abstract 클래스</b>
+ *
+ * <p>각 도메인의 서비스 예외 클래스는 해당 클래스를 extends 하여 구현한다.
+ *
+ */
+@Getter
+public abstract class BaseException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+
+    protected BaseException(CommonErrorCode errorCode, String customMsg) {
+        super(customMsg == null ? errorCode.getMessage() : customMsg);
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(CommonErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public static BaseException of(CommonErrorCode code) {
+        return new DefaultBaseException(code, null);
+    }
+
+    public static BaseException of(CommonErrorCode code, String msg) {
+        return new DefaultBaseException(code, msg);
+    }
+}
+
+class DefaultBaseException extends BaseException {
+    protected DefaultBaseException(CommonErrorCode errorCode) {
+        super(errorCode, null);
+    }
+    protected DefaultBaseException(CommonErrorCode errorCode, String customMsg) {
+        super(errorCode, customMsg);
+    }
+}
